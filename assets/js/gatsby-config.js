fe624ec4bfe2249dd697d89082645d5edc32d56e
yerimoh <bora3328@naver.com>
2021-05-25 23:55:38 +0900

    Create gatsby-config.js

---
+++ assets/js/gatsby-config.js
@@ -0,0 +1,13 @@
+plugins: [
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      // Footnotes mode (default: true)
+      footnotes: true,
+      // GitHub Flavored Markdown mode (default: true)
+      gfm: true,
+      // Plugins configs
+      plugins: [],
+    },
+  },
+],
